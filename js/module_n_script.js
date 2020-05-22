c1a9945a08d0f4018bc62da898927289d7490207
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-22 23:11:51 +0300

    JS quiz from Vasyl Manyo

---
+++ js/module_n_script.js
@@ -0,0 +1,62 @@
+// =================== no name =====================
+// var size = 3;
+// for (let i = 1; i < size; i++) {
+//   for (let k = 0; k < size - i; k++) {
+//     console.log(k);
+//   }
+//   for (let k = 0; k < i; k++) {
+//     console.log(k);
+//   }
+// }
+
+// =================== no name =====================
+// var students = ["John", "David"];
+// console.log(students["1"].length); // 5
+// console.log(students[1].length); // 5
+
+// =================== no name =====================
+// const num = Math.PI;
+// console.log(num.toFixed(2))   // 3.14
+
+// const num2 = (0.345 + 0.6458).toFixed(2)
+// console.log(num2)   // 0.99
+
+// =================== no name =====================
+// const myArray = ["zero", 3, 4, "five", 1, 2, 6, "nine", 8];
+// console.log(myArray["3"]);    // five
+
+// =================== no name =====================
+// var x = "Programming";
+// var z =
+//   x.substr(0, 6) +
+//   x.charAt(6).toUpperCase() +
+//   x.charAt(7).toUpperCase() +
+//   x.substr(8);
+// console.log(z);     // PrograMMing
+
+// console.log(x.substr(0, 6));    // Progra
+// console.log(x.slice(0, 6));     // Progra
+
+// =================== no name !!! =====================
+// function myFunc() {
+//   console.log(x);
+// }
+// myFunc();     // undefined
+// var x = 2;
+
+// =================== no name !!! =====================
+// var ls = [];
+// function f(r) {
+//   return r === 0 ? [] : [(ls[ls.length] = ls.length + 1), ...f(r - 1)];
+// }
+// console.log(f(5));      // [1,2,3,4,5]
+
+// =================== no name =====================
+// let a = typeof constructor == "object";     // false
+// let b = typeof constructor == "function";   // true
+// let c = typeof constructor == "undefined";  // false  
+// console.log(a, b, c);
+
+// =================== no name =====================
+
+// =================== no name =====================
